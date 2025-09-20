77fd9ddc4f101f196bbfab868925e2c98ba7553e
apponislam <11appon11@gmail.com>
2025-09-21 01:47:24 +0600

    Forget otp page created

--- src/app/(auth)/auth/forget-otp/page.tsx
+++ src/app/(auth)/auth/forget-otp/page.tsx
@@ -1,6 +1,5 @@
-import LoginForm from "@/components/forms/auth/login_form";
+import OtpForm from "@/components/forms/auth/forgot_pass_otp_form";
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
 const page = () => {
@@ -8,23 +7,13 @@ const page = () => {
         <div className="relative w-full h-screen">
             <div className="absolute inset-0 bg-[url('/auth_bg.jpg')] bg-cover bg-center opacity-50"></div>
             <div className="absolute inset-0 bg-black/50"></div>
-
             <div className="relative z-10 flex h-full flex-col lg:flex-row justify-between">
-                <div className="w-full md:w-full lg:w-1/2 py-4 px-4 md:pt-[100px] md:pl-[100px] md:pb-[100px]">
+                <div className="w-full md:w-full lg:w-1/2 py-4 px-4 pt-[100px] md:pl-[100px] md:pb-[100px] flex items-center justify-center">
                     <Image src="/logo.png" width={307} height={61} alt="Letanest Logo" className="mb-4 md:mb-6 h-11 w-auto md:h-[61px]"></Image>
-                    <h1 className="text-white text-3xl md:text-[57px] mb-5">
-                        Hosting made simple - <br className="hidden md:block" /> list your nest in minutes.
-                    </h1>
-                    <p className="text-xl md:text-[28px] text-white">
-                        if you don’t an account you can{" "}
-                        <Link href="/auth/register" className="text-[#135E9A]">
-                            Register here!
-                        </Link>
-                    </p>
                 </div>
 
                 <div className="w-full md:w-full lg:w-1/2 bg-[#14213D] rounded-tl-[40px] rounded-tr-[40px] lg:rounded-tl-[40px] lg:rounded-bl-[40px] lg:rounded-tr-none lg:rounded-br-none lg:px-[90px]">
-                    <LoginForm></LoginForm>
+                    <OtpForm></OtpForm>
                 </div>
             </div>
         </div>
