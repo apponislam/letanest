6b10724d92a531bcb914055429048e050ad338e1
apponislam <11appon11@gmail.com>
2025-09-28 23:07:57 +0600

    color issue and responsive simple issue fixed

--- src/components/home/Heroarea.tsx
+++ src/components/home/Heroarea.tsx
@@ -5,6 +5,7 @@ const Heroarea = () => {
     return (
         <section className="absolute inset-0 h-[900px] md:min-h-screen md:w-full z-0">
             <div className="absolute inset-0 bg-[url('/herobg.jpg')] bg-cover bg-center z-0" />
+            <div className="absolute inset-0 bg-black/45 rounded-xl"></div>
             {/* Optional overlay */}
             <div className="absolute inset-0 bg-black/20 z-10" />
             <div className=" container md:mx-auto p-4 relative z-20 min-h-full flex flex-col md:flex-row gap-6 mt-[120px] md:mt-[240px]">
@@ -13,7 +14,7 @@ const Heroarea = () => {
                     <p className="text-white text-xl md:text-3xl">Nest Anywhere with LetANest.</p>
                 </div>
                 <div className="w-full md:flex-1">
-                    <div className="w-full border border-[#C9A94D] py-9 px-5 backdrop-blur-md bg-[#C9A94D]/20">
+                    <div className="w-full border border-[#C9A94D] py-9 px-5 backdrop-blur-md bg-gradient-to-b from-[#14213D] via-[#14213D]/70 to-[#14213D]/40">
                         <h1 className="text-center font-semibold mb-3 text-3xl text-[#C9A94D] ">Find A Nest Anywhere</h1>
                         <p className="text-center text-[#C9A94D] mb-10">Your next adventure starts here - find a nest today.</p>
                         <HomeFilterForm></HomeFilterForm>
