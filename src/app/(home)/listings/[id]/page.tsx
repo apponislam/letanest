5a865918e9dc0a3f10c657e2870783af9e9d55c9
Appon Islam <appon2003@gmail.com>
2025-10-28 06:42:27 +0600

    Review now need approval

--- src/app/(home)/listings/[id]/page.tsx
+++ src/app/(home)/listings/[id]/page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 const page = () => {
     return (
         <>
-            {/* <PropertyPage></PropertyPage> */}
             <PropertyPage2></PropertyPage2>
         </>
     );
